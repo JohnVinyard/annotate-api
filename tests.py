cf34ed41f6e3f2e29756cca8d608b826fbef4e77
John Vinyard <john.vinyard@gmail.com>
2019-03-21 07:42:53 -0500

    Add back in a few more basic tests (now passing)

--- tests.py
+++ tests.py
@@ -176,38 +176,39 @@ class UserTests(BaseTests, unittest2.TestCase):
         self.assertEqual(6, len(items[-1]))
         self.assertEqual(96, sum(len(item) for item in items))
 
-    #
-    # def test_can_page_through_users_and_filter_by_user_type(self):
-    #     requesting_user, _ = self.create_user()
-    #
-    #     for _ in range(10):
-    #         self.create_user(user_type='human')
-    #
-    #     for _ in range(10):
-    #         self.create_user(user_type='featurebot')
-    #
-    #     resp = requests.get(
-    #         self.users_resource(),
-    #         params={'page_size': 3, 'user_type': 'featurebot'},
-    #         auth=self._get_auth(requesting_user))
-    #
-    #     self.assertEqual(client.OK, resp.status_code)
-    #     resp_data = resp.json()
-    #     self.assertEqual(3, len(resp_data['items']))
-    #     self.assertEqual(10, resp_data['total_count'])
-    #
-    #     items = [resp_data['items']]
-    #
-    #     while 'next' in resp_data:
-    #         current = requests.get(
-    #             self.url(resp_data['next']),
-    #             auth=self._get_auth(requesting_user))
-    #         resp_data = current.json()
-    #         items.append(resp_data['items'])
-    #
-    #     self.assertEqual(4, len(items))
-    #     self.assertEqual(1, len(items[-1]))
-    #     self.assertEqual(10, sum(len(item) for item in items))
+
+    def test_can_page_through_users_and_filter_by_user_type(self):
+        requesting_user, _ = self.create_user()
+
+        for _ in range(10):
+            self.create_user(user_type='human')
+
+        for _ in range(10):
+            self.create_user(user_type='featurebot')
+
+        resp = requests.get(
+            self.users_resource(),
+            params={'page_size': 3, 'user_type': 'featurebot'},
+            auth=self._get_auth(requesting_user))
+
+        self.assertEqual(client.OK, resp.status_code)
+        resp_data = resp.json()
+        self.assertEqual(3, len(resp_data['items']))
+        self.assertEqual(10, resp_data['total_count'])
+
+        items = [resp_data['items']]
+
+        while 'next' in resp_data:
+            current = requests.get(
+                self.url(resp_data['next']),
+                auth=self._get_auth(requesting_user))
+            resp_data = current.json()
+            items.append(resp_data['items'])
+
+        self.assertEqual(4, len(items))
+        self.assertEqual(1, len(items[-1]))
+        self.assertEqual(10, sum(len(item) for item in items))
+
     #
     # def test_bad_request_when_filtering_by_invalid_user_type(self):
     #     requesting_user, _ = self.create_user()
@@ -236,20 +237,21 @@ class UserTests(BaseTests, unittest2.TestCase):
     #         auth=self._get_auth(requesting_user))
     #     self.assertEqual(client.BAD_REQUEST, resp.status_code)
     #
-    # def test_can_view_most_data_about_self(self):
-    #     user1, user1_location = self.create_user()
-    #     resp = requests.get(
-    #         self.url(user1_location), auth=self._get_auth(user1))
-    #     self.assertIn('email', resp.json())
-    #     self.assertNotIn('password', resp.json())
-    #
-    # def test_can_view_limited_data_about_other_user(self):
-    #     user1, user1_location = self.create_user()
-    #     user2, user2_location = self.create_user()
-    #     resp = requests.get(
-    #         self.url(user1_location), auth=self._get_auth(user2))
-    #     self.assertNotIn('email', resp.json())
-    #     self.assertNotIn('password', resp.json())
+
+    def test_can_view_most_data_about_self(self):
+        user1, user1_location = self.create_user()
+        resp = requests.get(
+            self.url(user1_location), auth=self._get_auth(user1))
+        self.assertIn('email', resp.json())
+        self.assertNotIn('password', resp.json())
+
+    def test_can_view_limited_data_about_other_user(self):
+        user1, user1_location = self.create_user()
+        user2, user2_location = self.create_user()
+        resp = requests.get(
+            self.url(user1_location), auth=self._get_auth(user2))
+        self.assertNotIn('email', resp.json())
+        self.assertNotIn('password', resp.json())
     #
     # def test_can_view_most_data_about_self_when_listing_users(self):
     #     self.fail()
@@ -293,14 +295,15 @@ class UserTests(BaseTests, unittest2.TestCase):
     #     # more appropriate 404 Not Found error.
     #     self.assertEqual(client.FORBIDDEN, delete_resp.status_code)
     #
-    # def test_unauthorized_when_deleting_user_without_creds(self):
-    #     delete_resp = requests.delete(self.users_resource('1234'))
-    #     self.assertEqual(client.UNAUTHORIZED, delete_resp.status_code)
-    #
-    # def test_unauthorized_when_fetching_single_user_without_creds(self):
-    #     user1, user1_location = self.create_user()
-    #     user_resp = requests.get(self.url(user1_location))
-    #     self.assertEqual(client.UNAUTHORIZED, user_resp.status_code)
+
+    def test_unauthorized_when_deleting_user_without_creds(self):
+        delete_resp = requests.delete(self.users_resource('1234'))
+        self.assertEqual(client.UNAUTHORIZED, delete_resp.status_code)
+
+    def test_unauthorized_when_fetching_single_user_without_creds(self):
+        user1, user1_location = self.create_user()
+        user_resp = requests.get(self.url(user1_location))
+        self.assertEqual(client.UNAUTHORIZED, user_resp.status_code)
     #
     # def test_validation_error_for_bad_user_type(self):
     #     self.fail()
@@ -352,12 +355,13 @@ class UserTests(BaseTests, unittest2.TestCase):
     # def test_can_update_password(self):
     #     self.fail()
     #
-    # def test_not_found_for_non_existent_user(self):
-    #     user1, user1_location = self.create_user()
-    #     user_resp = requests.get(
-    #         self.users_resource('1234'), auth=self._get_auth(user1))
-    #     self.assertEqual(client.NOT_FOUND, user_resp.status_code)
-    #
-    # def test_unauthorized_when_fetching_non_existent_user_without_creds(self):
-    #     user_resp = requests.get(self.users_resource('1234'))
-    #     self.assertEqual(client.UNAUTHORIZED, user_resp.status_code)
+
+    def test_not_found_for_non_existent_user(self):
+        user1, user1_location = self.create_user()
+        user_resp = requests.get(
+            self.users_resource('1234'), auth=self._get_auth(user1))
+        self.assertEqual(client.NOT_FOUND, user_resp.status_code)
+
+    def test_unauthorized_when_fetching_non_existent_user_without_creds(self):
+        user_resp = requests.get(self.users_resource('1234'))
+        self.assertEqual(client.UNAUTHORIZED, user_resp.status_code)
