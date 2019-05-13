39cc275bba031c788f8cf484aaca8bb01192828d
John Vinyard <john.vinyard@gmail.com>
2019-05-13 17:16:04 -0500

    First stab at chroma and MFCC bots, with lots of code duplication to be gotten rid of

---
+++ examples/chroma_bot.py
@@ -0,0 +1,83 @@
+import requests
+import zounds
+from io import BytesIO
+import numpy as np
+from bot_helper import BinaryData, main, Listener
+
+N_FREQUENCY_BANDS = 512
+SAMPLE_RATE = zounds.SR11025()
+frequency_band = zounds.FrequencyBand(20, SAMPLE_RATE.nyquist)
+scale = zounds.MelScale(frequency_band, N_FREQUENCY_BANDS)
+FILTER_BANK_KERNEL_SIZE = 512
+FILTER_BANK = zounds.spectral.morlet_filter_bank(
+    SAMPLE_RATE,
+    FILTER_BANK_KERNEL_SIZE,
+    scale,
+    scaling_factor=np.linspace(0.1, 1.0, len(scale)),
+    normalize=True)
+FILTER_BANK *= zounds.AWeighting()
+FILTER_BANK = np.array(FILTER_BANK)
+
+
+class ChromaListener(Listener):
+    def __init__(self, client, s3_client, page_size=3):
+        super().__init__(client, s3_client, page_size)
+
+    def _process_sound(self, sound):
+        # fetch audio
+        resp = requests.get(sound['audio_url'])
+        raw_audio = BytesIO(resp.content)
+
+        # processing pipeline to compute spectrograms
+        samples = zounds.AudioSamples.from_file(raw_audio).mono
+        samples = samples.mono
+        samples = zounds.soundfile.resample(samples, SAMPLE_RATE)
+        windowing_sample_rate = zounds.SampleRate(
+            frequency=(FILTER_BANK_KERNEL_SIZE // 2) * SAMPLE_RATE.frequency,
+            duration=FILTER_BANK_KERNEL_SIZE * SAMPLE_RATE.frequency)
+        windowed = samples.sliding_window(windowing_sample_rate)
+        windowed = np.asarray(windowed)
+        spec = np.dot(FILTER_BANK, windowed.T).T
+        spec = np.abs(spec)
+        spec = 20 * np.log10(spec + 1)
+        spec = np.ascontiguousarray(spec).astype(np.float32)
+        spec = zounds.ArrayWithUnits(spec, [
+            zounds.TimeDimension(*windowing_sample_rate),
+            zounds.FrequencyDimension(scale)
+        ])
+
+        chroma_scale = zounds.ChromaScale(frequency_band)
+        chroma = chroma_scale.apply(spec, zounds.HanningWindowingFunc())
+        chroma = zounds.ArrayWithUnits(chroma, [
+            zounds.TimeDimension(*windowing_sample_rate),
+            zounds.IdentityDimension()
+        ]).astype(np.float32)
+
+        print(chroma.shape, chroma.dimensions)
+
+        binary_data = BinaryData(chroma)
+
+        # push output to s3
+        data_url = self.s3_client.put_object(
+            sound['id'],
+            binary_data.packed_file_like_object(),
+            'application/octet-stream')
+        print(f'pushed binary data to {data_url}')
+
+        # create annotation
+        self.client.create_annotations(sound['id'], {
+            'start_seconds': 0,
+            'duration_seconds': sound['duration_seconds'],
+            'data_url': data_url
+        })
+        print('created annotation')
+
+
+if __name__ == '__main__':
+    main(
+        user_name='chroma',
+        bucket_name='ChromaBot',
+        email='john.vinyard+chroma@gmail.com',
+        about_me='I compute chroma features!',
+        info_url='https://en.wikipedia.org/wiki/Chroma_feature',
+        listener_cls=ChromaListener)
---
+++ examples/mfcc_bot.py
@@ -0,0 +1,81 @@
+import requests
+import zounds
+from io import BytesIO
+import numpy as np
+from bot_helper import BinaryData, main, Listener
+from scipy.fftpack import dct
+
+
+N_FREQUENCY_BANDS = 512
+SAMPLE_RATE = zounds.SR11025()
+frequency_band = zounds.FrequencyBand(20, SAMPLE_RATE.nyquist)
+scale = zounds.MelScale(frequency_band, N_FREQUENCY_BANDS)
+FILTER_BANK_KERNEL_SIZE = 512
+FILTER_BANK = zounds.spectral.morlet_filter_bank(
+    SAMPLE_RATE,
+    FILTER_BANK_KERNEL_SIZE,
+    scale,
+    scaling_factor=np.linspace(0.1, 1.0, len(scale)),
+    normalize=True)
+FILTER_BANK *= zounds.AWeighting()
+FILTER_BANK = np.array(FILTER_BANK)
+
+
+class MFCCListener(Listener):
+    def __init__(self, client, s3_client, page_size=3):
+        super().__init__(client, s3_client, page_size)
+
+    def _process_sound(self, sound):
+        # fetch audio
+        resp = requests.get(sound['audio_url'])
+        raw_audio = BytesIO(resp.content)
+
+        # processing pipeline to compute spectrograms
+        samples = zounds.AudioSamples.from_file(raw_audio).mono
+        samples = samples.mono
+        samples = zounds.soundfile.resample(samples, SAMPLE_RATE)
+        windowing_sample_rate = zounds.SampleRate(
+            frequency=(FILTER_BANK_KERNEL_SIZE // 2) * SAMPLE_RATE.frequency,
+            duration=FILTER_BANK_KERNEL_SIZE * SAMPLE_RATE.frequency)
+        windowed = samples.sliding_window(windowing_sample_rate)
+        windowed = np.asarray(windowed)
+        spec = np.dot(FILTER_BANK, windowed.T).T
+        spec = np.abs(spec)
+        spec = 20 * np.log10(spec + 1)
+        spec = np.ascontiguousarray(spec).astype(np.float32)
+
+        mfcc = np.abs(dct(spec, axis=1)[:, 1: 14])
+
+        mfcc = zounds.ArrayWithUnits(mfcc, [
+            zounds.TimeDimension(*windowing_sample_rate),
+            zounds.IdentityDimension()
+        ]).astype(np.float32)
+
+        print(mfcc.shape, mfcc.dimensions)
+
+        binary_data = BinaryData(mfcc)
+
+        # push output to s3
+        data_url = self.s3_client.put_object(
+            sound['id'],
+            binary_data.packed_file_like_object(),
+            'application/octet-stream')
+        print(f'pushed binary data to {data_url}')
+
+        # create annotation
+        self.client.create_annotations(sound['id'], {
+            'start_seconds': 0,
+            'duration_seconds': sound['duration_seconds'],
+            'data_url': data_url
+        })
+        print('created annotation')
+
+
+if __name__ == '__main__':
+    main(
+        user_name='mfcc',
+        bucket_name='MFCCBot',
+        email='john.vinyard+mfcc@gmail.com',
+        about_me='I compute MFCCfeatures!',
+        info_url='https://en.wikipedia.org/wiki/Mel-frequency_cepstrum',
+        listener_cls=MFCCListener)
