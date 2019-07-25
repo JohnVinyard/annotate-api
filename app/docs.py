9e25abda751af0d61254d9059f07eeff995d6270
John Vinyard <john.vinyard@gmail.com>
2019-07-24 19:10:51 -0500

    Add a class to represent the docs for each resource

--- app/docs.py
+++ app/docs.py
@@ -1,5 +1,35 @@
 from app import Application
 from string import Formatter
+import inspect
+import ast
+
+
+class ResourceMethod(object):
+    def __init__(self, path, func):
+        super().__init__()
+        self.path = path
+        self.func = func
+
+    @property
+    def url_parameters(self):
+        formatter = Formatter()
+        return list(filter(
+            lambda x: x, (x[1] for x in formatter.parse(self.path))))
+
+    @property
+    def verb(self):
+        return self.func.__name__.split('_')[-1].upper()
+
+    @property
+    def description(self):
+        return self.func.__doc__
+
+    @property
+    def ast(self):
+        source = inspect.getsource(self.func).lstrip()
+        print(source)
+        tree = ast.parse(source)
+        print(tree)
 
 
 # TODO: list URL parameters from route
@@ -8,27 +38,20 @@ from string import Formatter
 # TODO: list example response bodies
 # TODO: list possible responses
 def generate_docs():
-    formatter = Formatter()
     methods = set(['on_get', 'on_post', 'on_head', 'on_patch', 'on_delete'])
 
     app = Application(None, None, None)
 
     for route, resource in app._doc_routes:
-        print('=================================')
-        url_params = list(filter(
-            lambda x: x, (x[1] for x in formatter.parse(route))))
-        print(route)
-        print(url_params)
 
         for item in dir(resource):
             if item not in methods:
                 continue
-
-            method = item.split('_')[-1].upper()
-            print(f'\t{method}')
-            func = getattr(resource, item).__doc__
-            if func:
-                print(f'\t{func}')
+            func = getattr(resource, item)
+            rm = ResourceMethod(route, func)
+            print('========================================')
+            print(rm.verb, rm.path, rm.url_parameters, rm.description)
+            print(rm.ast)
 
 
 if __name__ == '__main__':
