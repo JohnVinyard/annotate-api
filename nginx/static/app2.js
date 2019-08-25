617561ea6578b470be63a574433b46a5296a34aa
JohnVinyard <john.vinyard@gmail.com>
2019-08-24 19:16:28 -0500

    In the midst of major surgery on the sound view

--- nginx/static/app2.js
+++ nginx/static/app2.js
@@ -1,3 +1,4 @@
+
 class Authenticator {
   refreshUserIdentity(userName, password) {
     const client = new AnnotateApiClient(
@@ -31,6 +32,90 @@ const getApiClient = () => {
   const client = new AnnotateApiClient(
     identity.name, identity.password, cochleaAppSettings.apiHost);
   return client;
+};
+
+/*
+*
+*
+*/
+const featurePromise = (annotation, featureDataMapping, featureName) => {
+
+  // Check if we've already fetched features for this sound
+  let featureDataPromise = featureDataMapping[annotation.sound];
+
+
+  if (featureDataPromise === undefined) {
+    const apiClient = getApiClient();
+
+    // audio and features have not yet been fetched
+    featureDataPromise = apiClient
+      // Get sound data from the API
+      .getResource(apiClient.buildUri(annotation.sound))
+      // Fetch audio data from the remote audio url
+      .then(data => {
+        const audioUrl = data.low_quality_audio_url || data.audio_url;
+        return new Promise(function(resolve, reject) {
+          return fetchAudio(audioUrl, context).then(buffer => {
+            resolve({
+              buffer,
+              audioUrl,
+              soundUri: annotation.sound,
+              soundId: data.id
+             });
+          });
+        });
+      });
+
+      if(featureName === 'audio') {
+        // If the current feature being viewed is audio, we've already fetched
+        // it
+        featureDataPromise = featureDataPromise
+          .then(data => {
+            const {buffer, audioUrl, soundUri} = data;
+            const audioData = buffer.getChannelData(0);
+            const frequency = 1 / buffer.sampleRate;
+            const fd = new FeatureData(
+              audioData, [audioData.length], frequency, frequency);
+            return {featureData: fd, audioUrl};
+          });
+      } else {
+        // The feature being viewed is other than audio and needs to be fetched
+        featureDataPromise = featureDataPromise
+          .then(data => {
+            const {buffer, audioUrl, soundUri, soundId} = data;
+
+            const promise = getApiClient().getSoundAnnotationsByUser(
+              soundId, app.currentFeature.id);
+            return promiseContext(promise, r => ({audioUrl}));
+          })
+          .then(result => {
+            const {data, audioUrl} = result;
+            const promise = fetchBinary(data.items[0].data_url);
+            return promiseContext(promise, r => ({audioUrl}));
+          })
+          .then(result => {
+            const {data, audioUrl} = result;
+            const featureData = unpackFeatureData(data);
+            return {featureData, audioUrl};
+          });
+      }
+
+    // Put the pending promise into the map
+    featureDataMapping[annotation.sound] = featureDataPromise;
+  }
+
+  const slicedPromise = featureDataPromise.then(data => {
+    const {featureData, audioUrl} = data;
+    return featureData.timeSlice(
+      annotation.start_seconds, annotation.duration_seconds);
+  });
+
+  const audioUrlPromise = featureDataPromise.then(data => {
+    const {featureData, audioUrl} = data;
+    return audioUrl;
+  });
+
+  return [slicedPromise, audioUrlPromise];
 };
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -76,20 +161,36 @@ document.addEventListener('DOMContentLoaded', function() {
         isVisible: false,
         zoom: 1,
         featureData: null,
-        scrollListener: null
+        scrollListener: null,
+        panListener: null,
+        resizeListener: null
       };
     },
     methods: {
       zoomIn: function() {
         this.zoom = Math.min(20, this.zoom + 1);
+        this.draw();
       },
       zoomOut: function() {
         this.zoom = Math.max(1, this.zoom - 1);
+        this.draw();
+      },
+      draw:  function() {
+        const drawContext = this.$refs.canvas.getContext('2d');
+        // drawContext.fillRect(Math.random() * 100, Math.random() * 100, 10, 10);
+        drawContext.fillText(this.featureData, 10, 50);
+      },
+      draw1D: function() {
+
+      },
+      draw2D: function() {
+
       }
     },
     destroyed: function() {
-      console.log('destroyed');
       document.removeEventListener('scroll', this.scrollListener[0]);
+      this.$refs.container.removeEventListener('scroll', this.panListener);
+      window.removeEventListener('resize', this.resizeListener);
     },
     mounted: function() {
       const [checkVisibility, promise] = scrolledIntoView(this.$refs.container);
@@ -97,9 +198,24 @@ document.addEventListener('DOMContentLoaded', function() {
       promise
         .then(() => {
           this.isVisible = true;
-          const drawContext = this.$refs.canvas.getContext('2d');
-          // drawContext.fillRect(Math.random() * 100, Math.random() * 100, 10, 10);
-          drawContext.fillText(this.annotation.sound, 10, 50);
+          const [slicedPromise, audioUrlPromise] =
+            this.annotation.featurePromise();
+
+            slicedPromise.then(featureData => {
+              this.featureData = featureData;
+
+              // render for the first time once scrolled into view and feature data
+              // is loaded
+              this.draw();
+
+              // re-draw whenever the scroll position changes
+              this.panListener =
+                onScroll(this.$refs.container, () => this.draw(false), 100);
+
+              // re-draw whenever the window is resized
+              this.resizeHandler =
+                onResize(window, () => this.draw(true), 100);
+            });
         });
     },
   });
@@ -123,7 +239,6 @@ document.addEventListener('DOMContentLoaded', function() {
               const fp = () => featurePromise(
                 annotation,
                 featureDataMapping,
-                searchResults,
                 annotation.sound);
               annotation.featurePromise = fp;
             });
@@ -132,14 +247,7 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     },
     mounted: function() {
-      const identity = auth.tryGetUserIdentity();
-      const client = new AnnotateApiClient(
-        identity.name, identity.password, cochleaAppSettings.apiHost);
-      client
-        .getAnnotations()
-        .then(data => {
-          this.annotations = data.items;
-        });
+      this.handleSubmit();
     }
   });
 
