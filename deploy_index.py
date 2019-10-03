22e5fc29083960ba2dfb3e38190d8cef267e3cef
John Vinyard <john.vinyard@gmail.com>
2019-10-03 18:52:21 -0500

    Fully-working deploy script for spatial index API

--- deploy_index.py
+++ deploy_index.py
@@ -2,7 +2,7 @@ import argparse
 from deploygraph import Requirement, AlwaysUpdateException
 from deploygraph.aws import \
     PublicInternetSecurityGroup, Box, AnacondaServer, SupervisordHelper, DNS, \
-    TLS
+    TLS, NamedStringIO
 
 
 class IndexBox(Box):
@@ -10,16 +10,21 @@ class IndexBox(Box):
         super().__init__(
             'embedding-index-3d', linux_image, 't2.small', security_group)
 
-# TODO: nginx
+
 class IndexServer(AnacondaServer):
-    def __init__(self, box, port, config):
+    def __init__(self, box, port, config, domain_name):
         super().__init__(box, None, 'embedding-index-3d')
+        self.domain_name = domain_name
         self.config = config
         self.port = port
+        self.box = box
         self.supervisord = SupervisordHelper(
             'embedding-index-3d',
             'examples',
             'remote/')
+
+    def connection(self):
+        return self.box.connection()
 
     def fulfilled(self):
         """
@@ -40,7 +45,7 @@ class IndexServer(AnacondaServer):
             'supervisord_helper': self.supervisord,
             'config': self.config,
             'PublicIpAddress': data['PublicIpAddress'],
-            'PublicDnsName': data['PublicDnsName']
+            'PublicDnsName': data['PublicDnsName'],
         }
 
     def _inner_fulfill(self, connection):
@@ -48,11 +53,37 @@ class IndexServer(AnacondaServer):
         connection.put('examples/spatial_index_api.py', 'remote/')
         connection.put('examples/hyperplane_tree.py', 'remote/')
         connection.put('examples/log.py', 'remote/')
+
+        # nginx setup
+        connection.sudo('wget http://nginx.org/keys/nginx_signing.key')
+        connection.sudo('apt-key add nginx_signing.key')
+
+        connection.append_line(
+            'deb http://nginx.org/packages/ubuntu xenial nginx',
+            '/etc/apt/sources.list')
+        connection.append_line(
+            'deb-src http://nginx.org/packages/ubuntu xenial nginx',
+            '/etc/apt/sources.list')
+
+        self.apt_install(connection, 'nginx')
+        with open('examples/nginx.conf', 'r') as f:
+            content = f.read()
+            with_replacements = content.format(
+                server_name=self.domain_name,
+                upstream=f'localhost:{self.port}')
+            sio = NamedStringIO('nginx.conf', with_replacements)
+            connection.sudo_put(sio, '/etc/nginx')
+
         self.supervisord.prepare(connection, variables=self.config)
         with self._conda_env(connection):
             connection.run(
                 'pip install falcon gunicorn supervisor')
             self.supervisord.start(connection)
+
+        # nginx start
+        connection.sudo('nginx -t -c /etc/nginx/nginx.conf')
+        connection.sudo('service nginx start')
+        connection.sudo('service nginx reload')
 
 
 class IndexApp(Requirement):
@@ -77,6 +108,9 @@ class IndexApp(Requirement):
     def supervisord(self):
         return self.server.supervisord
 
+    def connection(self):
+        return self.server.connection()
+
     def fulfill(self):
         connection = self.server.connection()
         data = self.server.data()
@@ -86,6 +120,7 @@ class IndexApp(Requirement):
         self.supervisord.copy_config(connection, variables=data['config'])
         with self._conda_env(connection):
             self.supervisord.restart(connection)
+        connection.sudo('service nginx reload')
 
 
 if __name__ == '__main__':
@@ -116,8 +151,8 @@ if __name__ == '__main__':
     public_internet = PublicInternetSecurityGroup()
     index_box = IndexBox(linux_image, public_internet)
     index_server = IndexServer(
-        index_box, args.internal_port, supervisor_config_vars)
+        index_box, args.internal_port, supervisor_config_vars, domain)
     index_app = IndexApp(index_server)
-    dns = DNS(index_app, domain)
-    tls = TLS(dns, args.tls_email)
+    dns = DNS(index_app, domain, add_www=False)
+    tls = TLS(dns, args.tls_email, add_www=False)
     tls()
